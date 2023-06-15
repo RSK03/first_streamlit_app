158b95a50de6181e0f34a6926a937bf35cacca87
Sai kishore <88890438+RSK03@users.noreply.github.com>
2023-06-16 00:36:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('RSK-BOSS')
-streamlit.header('RSK-CEO')
-streamlit.text('RSK-EMP')
-streamlit.text('RSK-BOSS')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
