5111dff5f46e0ac5b9d4a4008bf91e504045f95b
Sai kishore <88890438+RSK03@users.noreply.github.com>
2023-06-16 00:37:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🍌🥭Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥝🍇Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled 🥝🍇Free-Range Egg')
